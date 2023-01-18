94a9c2273e022374c22fbc57c6f4392d362267be
James Westbrook <jamesmw732@gmail.com>
2023-01-18 14:17:13 -0500

    first commit

---
+++ calc.py
@@ -0,0 +1,125 @@
+from re import split
+
+class Calc:
+    def __init__(self, pname, inactivity):
+        self.pname = pname
+        self.namelist = pname.split()
+        self.inactivity = inactivity
+        self.damage = 0
+        self.damagelist = []
+        self.times = [-1, -1]
+        self.path = ""
+        self.lastLine = 0
+    
+    def setName(self, name):
+        self.pname = name
+        self.namelist = name.split()
+
+    def reset(self): # resets damage stats to zero
+        self.damage = 0
+        self.damagelist.clear()
+        self.times = [-1, -1]
+        return
+    
+    def updateTime(self, time):
+        if self.times[0] == -1: # data is empty
+            self.times[0] = time
+            self.times[1] = time
+        elif time - self.times[1] > self.inactivity: # inactivity triggered
+            self.reset()
+            self.times[0] = time
+            self.times[1] = time
+        else: # no inactivity triggered, typical case
+            self.times[1] = time
+
+
+    def resetLastLine(self):
+        self.lastLine = 0
+
+    def setLastLine(self): # set new last line
+        with open(self.path) as gamelog:
+            self.lastLine = len(gamelog.readlines())-1
+        gamelog.close()
+        return
+
+    def getLines(self):
+        lines = []
+        with open(self.path) as gamelog:
+            allLines = gamelog.readlines()
+            if self.lastLine > len(allLines): # hail mary for when game.txt gets replaced by a blank game.txt
+                lines = allLines
+            else:
+                lines = allLines[self.lastLine:len(allLines) - 1]
+            self.lastLine = len(allLines) - 1
+        gamelog.close()
+        return lines
+
+    def elapsedTime(self):
+        return self.times[-1] - self.times[0]
+    
+    def updateStats(self): # given lines from game.txt, 
+        lines = self.getLines()
+        if len(lines) == 0:
+            return
+        for line in lines:
+            damage = ""
+            lineArr = split(" for | damage!", line) # an array containing mostly junk, then damage, then junk
+            if len(lineArr) != 3: # if it wasn't split three ways, then we can toss out this line
+                continue
+            try:
+                damage = lineArr[1]
+            except IndexError:
+                continue
+            try:
+                damage = int(damage)
+                splitline = lineArr[0].split(" ")
+                validname = True
+                for nameind in range(2, 2 + len(self.namelist)): # check if name is valid
+                    if splitline[nameind] != self.namelist[nameind - 2]:
+                        validname = False
+                # at this point, we were able to extract a damage, and the damage was coming from the player
+                if validname:
+                    curTime = getTime(line)
+                    self.updateTime(curTime)
+                    self.damage += damage
+                    self.damagelist.append(damage)
+            except ValueError:
+                continue
+        return
+
+    def out(self):
+        return (self.damage, self.damagelist, self.elapsedTime())
+
+# helper methods
+def getTime(line): # get the time, in seconds, of the given line
+    if len(line) < 8:
+        return -1
+    hours = line[0] + line[1]
+    mins = line[3] + line[4]
+    secs = line[6] + line[7]
+    try:
+        hours = int(hours)
+        mins = int(mins)
+        secs = int(secs)
+        return int(hours) * 3600 + int(mins) * 60 + int(secs)
+    except ValueError:
+        return -1
+
+def getLastLine(f):
+    with open(f) as file:
+        l = len(file.readlines()) - 1
+    file.close()
+    return l
+
+# if __name__ == "__main__":
+#     calc = Calc("Kaiman", 10)
+
+#     f1 = "testinput.txt"
+#     f2 = "testinput2.txt"
+
+#     calc.path = f1
+#     calc.updateStats()
+
+#     calc.path = f2
+#     calc.updateStats()
+#     print(calc.out())
---
+++ main.py
@@ -0,0 +1,114 @@
+from tkinter import *
+from tkinter import ttk
+from colors import *
+from calc import *
+
+def start(calc):
+    for entry in entries:
+        entry.delete(0, END)
+        entry.insert(0, 0)
+    global track
+    track = True
+    calc.setLastLine()
+    calc.setName(namebox.get())
+    update(calc)
+
+def update(calc): # update the state of the calc and GUI based on new game log content
+    if not track:
+        return
+
+    global tracklabel
+    tracklabel.config(text="Tracking DPS")
+
+    lastline = getLastLine(calc.path)
+    if lastline != calc.lastLine:
+        calc.updateStats()
+        if calc.damage > 0:
+            clearEntries()
+            displayStats(calc)
+    root.after(1000, lambda: update(calc))
+
+def displayStats(calc): # displays current state of the calc to the GUI
+    Label(root, width=10, bg=grey, fg=text).grid(column=4, row=0)
+    entries[0].insert(0, calc.damage) # total damage
+    dmgList = calc.damagelist
+    entries[1].insert(0, len(dmgList)) # number of hits
+    entries[2].insert(0, min(dmgList)) # min hit
+    entries[3].insert(0, max(dmgList)) # max hit
+    entries[4].insert(0, round(int(entries[0].get())/int(entries[1].get())))  # avg hit
+    elapsedTime = calc.elapsedTime()
+    entries[5].insert(0, elapsedTime) # elapsed time
+    if elapsedTime > 0:
+        entries[6].insert(0, round(int(calc.damage) // (elapsedTime))) # DPS
+        entries[7].insert(0, int(entries[6].get()) * 60) # DPM
+    else:
+        entries[6].insert(0, "N/A")
+        entries[7].insert(0, "N/A")
+
+
+def interrupt(): # stop tracking damage
+    global track
+    global tracklabel
+    track = False
+    tracklabel.config(text="")
+
+def clearEntries(): # clear the stat entries, used when you first click "start"
+    for entry in entries:
+        entry.delete(0, END)
+
+def createInputFrame(container, calc):
+    frame = ttk.Frame(container)
+    Label(container, text='Name:', bg=grey, fg=text).grid(column=0, row=0)
+    global namebox
+    namebox = Entry(container, width=12, bg=grey, fg="cyan")
+    namebox.insert(0, calc.pname)
+    namebox.focus()
+    namebox.grid(column=1, row=0)
+    Button(container, text='Start', activebackground=hoverBG, activeforeground=hoverText, bg=brown, command= lambda: start(calc)).grid(column=2, row=0)
+    Button(container, text='Stop', activebackground=hoverBG, activeforeground=hoverText, bg=brown, command=interrupt).grid(column=3, row=0)
+    return frame
+
+def createOutputFrame(container):
+    frame = ttk.Frame(container)
+    cols = ["Damage", "# Hits", "Min Hit", "Max Hit", "Avg Hit", "Time (s)", "DPS", "DPM"]
+    for i in range(8):
+        Label(container, text=cols[i], bg=grey, fg=text).grid(column=i,row=1)
+    for j in range(8):
+        entries[j].grid(column=j,row=2)
+    
+    return frame
+
+def createMainWindow(calc):
+    createOutputFrame(root)
+    createInputFrame(root, calc)
+    root.mainloop()
+    return
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("DPS Calc")
+    root.configure(bg="#282828")
+
+    track = False
+    entries = [Entry(root, width=12, bg=grey, fg=text) for _ in range(8)]
+    namebox = None
+
+    tracklabel = Label(root, text='', bg=grey, fg=text)
+    tracklabel.grid(column=4, row=0)
+
+    settings = open("settings.txt", "r")
+    data = []
+    for line in settings:
+        data.append(line)
+    name = data[0].strip() # exclude newline character
+    gamelog = data[1].strip() # game.txt path, contains all messages sent to game window
+    try:
+        inactivity = int(data[2])
+    except ValueError:
+        inactivity = 10
+    except IndexError:
+        inactivity = 10
+    calc = Calc(name, inactivity)
+    calc.path = gamelog
+
+    createMainWindow(calc)
