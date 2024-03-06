44f018d049bfa841cd663718e538206741f040c0
jamesw732 <jamesmw732@gmail.com>
2024-03-05 19:38:42 -0500

    Added some colors

--- colors.py
+++ colors.py
@@ -4,4 +4,4 @@ text_color = "#8c8a84"
 clicked = "#3a3428"
 hoverText = "#c9c86f"
 hoverBG = "#544d3a"
-colorwheel = ['red', 'blue', 'green', 'cyan', 'salmon', 'white', 'purple', 'orange', 'yellow', 'PaleTurquoise', 'gold']
+colorwheel = ['red', 'royalblue', 'green', 'cyan', 'salmon', 'white', 'orange', 'yellow', 'lawngreen', 'magenta']
